3f00bff4b2e324ef3a3a5202164870dfc1db3b6c
Dimitri Gontscharenko <dima.webdevelopment@gmail.com>
2023-05-18 17:43:56 +0200

    create data protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 const routes: Routes = [
   { path: "", component: SlideshowComponent },
-  { path: "legal-notice", component: LegalNoticeComponent }
+  { path: "legal-notice", component: LegalNoticeComponent },
+  { path: "data-protection", component: DataProtectionComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MenuComponent } from './menu/menu.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SlideshowComponent,
     MenuComponent,
-    LegalNoticeComponent
+    LegalNoticeComponent,
+    DataProtectionComponent
   ],
   imports: [
     BrowserModule,
